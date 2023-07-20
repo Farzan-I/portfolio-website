a9bfbc9ea361a82ea02ca3ada1ee78fcbc8d4700
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-07-20 20:41:37 +0100

    Add heading to tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,17 +1,28 @@
 import { BallCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { technologies } from '../constants';
+import { motion } from 'framer-motion';
+
+import {styles } from '../styles'
+import { textVariant } from '../utils/motion';
 
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      { technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon}/>
-        </div>
-      ))}
-    </div>
+    <>
+      <motion.div variants={textVariant()}>
+        <h1 className={`${styles.sectionHeadText} text-center`}>Techs</h1>
+          <p className={`${styles.sectionSubText} text-center`}>What I have experience using</p>
+        </motion.div>
+      
+      <div className="mt-10 flex flex-row flex-wrap justify-center gap-10">
+        { technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name}>
+            <BallCanvas icon={technology.icon}/>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
