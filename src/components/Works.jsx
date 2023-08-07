bf469d8529c787dec4c0f1a507e32d6e62eff67b
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-08-07 18:22:43 +0100

    Add youtube badge to portfolio cards

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,11 +3,12 @@ import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { github } from '../assets';
+import { youtube } from '../assets';
 import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 
-const ProjectCard = ({ index, name, description, tags, image, source_code_link}) => {
+const ProjectCard = ({ index, name, description, tags, image, source_code_link, source_code_link_youtube}) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt
@@ -32,6 +33,16 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link})
               <img 
                 src={github}
                 alt="github"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+            <div 
+              onClick={() => window.open (source_code_link_youtube, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img 
+                src={youtube}
+                alt="youtube"
                 className="w-1/2 h-1/2 object-contain"
               />
             </div>
@@ -68,10 +79,10 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-          Following projects showcases my skills and experience through
+          Following projects showcase my skills and experience through
           real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
+          links to code repositories and live demos. It reflects my
+          ability to solve complex problems, work with different technologies
           and manage projects effectively.
         </motion.p>
       </div>
