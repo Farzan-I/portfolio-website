216b6237b69102c1621e0cdc717fb4bd1c8b5aca
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-08-12 00:25:18 +0100

    Change styling of title, make punctuation coloured

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,8 +13,8 @@ const Hero = () => {
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white text-outline`}>Hi, I&apos;m <span className="text-[#FE0039]">Farzan</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100 text-outline`}>A recent career changer with a background in business 
+          <h1 className={`${styles.heroHeadText} text-white text-outline`}>Hi<span className="text-[#FE0039]">,</span> I<span className="text-[#FE0039]">&apos;</span>m Farzan</h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100 text-outline-small`}>A recent career changer with a background in business 
             looking for my first role as a developer! <br className="sm:block hidden"/>
           </p>
           <br className="sm:block hidden"/>
