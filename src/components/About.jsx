eca038ff46f644f3f2de25e42d30f317d8ac7d32
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-09-13 18:23:11 +0330

    Rearrange service cards, reword introduction

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -39,9 +39,15 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
       </motion.div>
 
+      <div className="mt-10 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+
       <motion.p 
         variants={fadeIn("", "", 0.1, 1)} 
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        className="mt-10 text-white text-[17px] max-w-3xl leading-[30px]"
       >
         First of all, allow me to thank you for visiting my website. A lot of blood, sweat, caffeine and tears went into making this happen! 
         I hope you have a great experience and learn exactly who the person behind the name (that most people don&apos;t know how to pronounce) is and what I&apos;m all about.
@@ -49,25 +55,18 @@ const About = () => {
         <br/>
         <br/>
 
-        Coming from a business background with extensive management experience, 
-        I&apos;ve worked in numerous full agile teams and have collaborated on several projects using 
-        various technologies. My fascination for coding started while in my first role after university, but I never took it seriously as imposter syndrome kicked in very early and I pursued other careers.
+        With extensive business management experience, I&apos;ve worked in full agile teams and collaborated on various projects using various technologies. Most can be found below. 
+        My fascination with coding began in my graduate role, but I pursued other careers as imposter syndrome kicked in early.
 
         <br/>
         <br/>
 
-        Only in my role as a marketing executive did I realise that I had a natural knack for software engineering. Here I put my doubts aside and helped create the  
+        As a marketing executive, I discovered my natural talent for software engineering. I helped create the 
         <a href="https://www.culturalsignatures.com/" rel="noreferrer" target="_blank">
-          <span className="text-white"> company&apos;s website</span>
-        </a>. I enjoyed working on the website so much that I spent my spare time studying HTML, CSS, Vanilla JavaScript and Ruby. After the Covid-19 pandemic, I joined one of Europe&apos;s leading coding 
-        bootcamps to expand on my new found knowledge, where I got to grips with what it takes to be a professional software engineer (mostly surfing Google for answers and not being afraid to break your code!). I have been freelancing ever since graduating and looking forward to starting my first professional role as an engineer!
-      </motion.p>
-
-      <div className="mt-20 flex flex-wrap gap-10">
-        {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service} />
-        ))}
-      </div>
+          <span className="text-secondary"> company&apos;s website</span>
+        </a> and enjoyed it so much that I used my spare time studying HTML, CSS, JavaScript, and Ruby. After the Covid-19 pandemic, I joined a leading coding bootcamp to expand my knowledge and become a professional software engineer. Since graduating, 
+        I&apos;ve been freelancing and eager to start my first professional role as an engineer!
+       </motion.p>
     </>
   )
 }
