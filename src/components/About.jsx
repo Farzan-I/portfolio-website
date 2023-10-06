5b4c72125ad899a9d513c0b61d17556cc77d6b8f
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-10-07 02:09:29 +0330

    Reword about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,7 +22,7 @@ const ServiceCard = ({ index, title, icon }) => {
           className="bg-card rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
           <img src={icon} alt={title} className="w-16 h-16 object-contain" />
-          <h3 className="text-white text-[20px] font-bold text-center justify">
+          <h3 className="text-white-100 text-[20px] font-bold text-center justify">
             {title}
           </h3>
         </div>
@@ -47,7 +47,7 @@ const About = () => {
 
       <motion.p 
         variants={fadeIn("", "", 0.1, 1)} 
-        className="mt-10 text-white text-[17px] max-w-3xl leading-[30px]"
+        className="mt-10 text-white-100 text-[17px] max-w-3xl leading-[30px]"
       >
         First of all, allow me to thank you for visiting my website. A lot of blood, sweat, caffeine and tears went into making this happen. 
         I hope you have a great experience and learn exactly who the person behind the name that most people don&apos;t know how to pronounce is!
@@ -55,18 +55,24 @@ const About = () => {
         <br/>
         <br/>
 
-        The path that led me to this point was not always so straight forward. While working in management, marketing and financial roles, I noticed the ever changing world 
+        The path that led me to this point was never straight forward. A lot of self-doubt, disbelief and delaying forced me to pursue other career avenues.
+        My fascination with coding did however begin in my graduate role. Imposter syndrome did kick in early and I dismissed the idea of becoming an engineer.
         
-         I&apos;ve worked in full agile teams and collaborated on various projects using a wide range of technologies. Most can be found below. 
-        My fascination with coding began in my graduate role, but I pursued other careers as imposter syndrome kicked in early.
-
+        While working in management, marketing and financial roles, I still had hopes of knowing how to efficiently use the terminal on a computer like a pro but still considered it only a dream. 
+        It was only until my role as a marketing that I found my natural knack for software engineering.
+        
         <br/>
         <br/>
 
-        As a marketing executive, I discovered my natural talent for software engineering. I helped create the 
+        Here I helped create the 
         <a href="https://www.culturalsignatures.com/" rel="noreferrer" target="_blank">
           <span className="text-secondary"> company&apos;s website</span>
-        </a> and enjoyed it so much that I used my spare time studying HTML, CSS, JavaScript, and Ruby. After the Covid-19 pandemic, I joined a leading coding bootcamp to expand my knowledge and become a professional software engineer. Since graduating, 
+        </a> and enjoyed it so much that I used my spare time studying HTML, CSS, JavaScript, and Ruby. After the Covid-19 pandemic, I joined a 
+        <a href="https://www.makers.tech/" rel="noreferrer" target="_blank">
+          <span className="text-secondary"> leading coding bootcamp</span>
+          </a> to expand my knowledge and actualise my dream of becoming a software engineer. 
+        I have experience of working in full agile teams and have collaborated on various projects using a wide range of technologies. Most can be found below. 
+
         I&apos;ve been freelancing and eager to start my first professional role as an engineer!
        </motion.p>
     </>
