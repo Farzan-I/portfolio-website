e4467f4dc00a0479a2494efe728ec868643843b7
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-11-13 14:19:49 +0330

    Remove shadow from project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,7 @@ const ProjectCard = ({ index, name, description, tags, image, otherImage, source
           scale: 1,
           speed: 450
         }}
-        className="red-gradient p-5 rounded-2xl sm:w-[360px] w-full shadow-card"
+        className="red-gradient p-5 rounded-2xl sm:w-[360px] w-full"
       >
         <div className="relative w-full h-[230px]">
           <img 
