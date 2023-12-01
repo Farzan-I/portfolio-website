c80612ba980abf4cbbfe038904877f78f19e2889
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-12-01 22:55:52 +0000

    Add instruction for hero scoller (press), lower scroller on screen

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
       
       <ComputersCanvas />
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-10 bottom-10 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[84px] rounded-3xl border-4 border-white-100 flex justify-center items-start p-2">
             <motion.dev 
@@ -39,6 +39,11 @@ const Hero = () => {
               }}
               className="w-3 h-3 rounded-full bg-white-100 mb-1"
             />
+          </div>
+          <div className='absolute items-center'>
+            <p>
+                Press!
+            </p>
           </div>
         </a>
       </div>
