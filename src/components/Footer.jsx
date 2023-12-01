6ae8a424a9c450c96c4710060e877cdf2585851c
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-12-01 22:54:46 +0000

    Add footer with links to social media and disclaimer with links to sketch fab credits

---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import { styles } from '../styles';
+import {} from '../assets';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+
+  return (
+    <foot>
+    
+      <div className={`${styles.paddingX} w-full flex items-center py-5 bottom-0 z-20 justify-end bg-quinary gap-3`}>
+        <div className={`${styles.paddingX} w-full flex `}>
+
+          <p className="text-white-100 text-[10px]">
+            <a href="https://skfb.ly/orSpX/" rel="noreferrer" target="_blank">
+              <span className="text-secondary">MacBook Pro</span>
+            </a> model by 
+            <a href="https://sketchfab.com/KangaroOz-3D" rel="noreferrer" target="_blank">
+              <span className="text-secondary"> KangaroOz 3D </span>
+            </a> is licensed under 
+            <a href="http://creativecommons.org/licenses/by-nc/4.0/" rel="noreferrer" target="_blank">
+              <span className="text-secondary"> Creative Commons Attribution-NonCommercial</span>
+            </a>. All product and company names are trademarks™️ or registered®️ trademarks of their respective holders.
+            Use of them does not imply any affiliation with or endorsement by them.
+          </p>
+        </div>
+        <div className={`${styles.paddingX} flex items-center gap-1`}>
+          <Link to="https://github.com/Farzan-I" target="_blank" rel="noreferrer">
+            <img src="../src/assets/github.png" alt="GitHub" width={50} height={50}/>
+          </Link>
+          <Link to="https://www.codewars.com/users/fazzy15" target="_blank" rel="noreferrer">
+            <img src="../src/assets/codewars.png" alt="Codewars" width={40} height={40}/>
+          </Link>
+          <Link to="https://www.linkedin.com/in/farzan-imanzadeh-079a95a3/" target="_blank" rel="noreferrer">
+            <img src="../src/assets/linkedin.png" alt="LinkedIn" width={50} height={50}/>
+          </Link>
+        </div>
+      </div>
+    </foot>
+  )
+}
+
+export default Footer
