02018418071f7f668e03cdb077eac86f7b9fa660
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-12-10 10:47:49 +0000

    Add navbar wrapper, toggle state, add hamburger class for navbar menu, navbar menu can now close when anywhere else on the page is clicked

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,59 +5,56 @@ import { styles } from '../styles';
 import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
-const Navbar = () => {
+const Navbar = ({toggle, setToggle}) => {
   const [active, setActive] = useState('');
-  const [toggle, setToggle] = useState(false);
-  
-
   return (
-    <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-navbar`}
-    >
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-       <div className="flex-col">
-        <Link 
-            to="/" 
-            className="flex items-center gap-2" 
-            onClick={() => {
-              setActive("");
-              window.scrollTo(0, 0);
-            }}
-          >
-            <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-            <div className='flex flex-col'>
-              <p className="text-white-100 text-[18px] font-bold cursor-pointer"> Farzan Imanzadeh </p>
-              <span className="text-sm">Full-Stack Engineer</span>
-            </div>
-          </Link>
-       </div>
+    <div className='navbar-wrapper'>
+      <img src={toggle ? close : menu} alt="menu" className="hamburger w-[21px] h-[21px] object-contain cursor-pointer bg-black" onClick={() => setToggle(!toggle)}/>
+        <div className={`${!toggle ? 'hidden' : 'flex' } p-6 black-gradient fixed top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
+          <ul className="list-none flex justify-end items-start flex-col gap-4">
+            {navLinks.map((link) => (
+              <li
+                key={link.id}
+                className={`${
+                  active === link.title
+                  ? "text-secondary"
+                  : "text-white-100"
+                } hover:text-secondary font-poppins font-medium cursor-pointer text-[18px]`}
+                onClick={() => {
+                  setToggle(!toggle)
+                  setActive(link.title)
+                }}
+              >
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
         
-    
-        <div className="flex flex-1 justify-end items-center">
-          <img src={toggle ? close : menu} alt="menu" className="w-[21px] h-[21px] object-contain cursor-pointer bg-black" onClick={() => setToggle(!toggle)}/>
-          <div className={`${!toggle ? 'hidden' : 'flex' } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
-            <ul className="list-none flex justify-end items-start flex-col gap-4">
-              {navLinks.map((link) => (
-                <li
-                  key={link.id}
-                  className={`${
-                    active === link.title
-                    ? "text-secondary"
-                    : "text-white-100"
-                  } hover:text-secondary font-poppins font-medium cursor-pointer text-[18px]`}
-                  onClick={() => {
-                    setToggle(!toggle)
-                    setActive(link.title)
-                  }}
-                >
-                  <a href={`#${link.id}`}>{link.title}</a>
-                </li>
-              ))}
-            </ul>
+      <nav
+        className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-navbar`}
+        onClick={() => setToggle(false)}
+      >
+        <div className="w-full flex justify-between items-center mx-auto">
+          <div className="flex-col">
+            <Link 
+                to="/" 
+                className="flex items-center gap-2" 
+                onClick={() => {
+                  setActive("");
+                  window.scrollTo(0, 0);
+                }}
+              >
+                <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+                <div className='flex flex-col'>
+                  <p className="text-white-100 text-[18px] font-bold cursor-pointer"> Farzan Imanzadeh </p>
+                  <span className="text-sm text-white-100">Full-Stack Engineer</span>
+                </div>
+              </Link>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   )
 }
 
