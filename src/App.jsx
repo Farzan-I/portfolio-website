e651ac8c22e69ab548e69a36e554cb2e2bd31c52
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-12-10 10:51:14 +0000

    Add toggle state to navbar component in App.jsx file to allow menu close when clicking anywhere on the screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import { BrowserRouter } from "react-router-dom"
-
+import { useState } from "react";
 import { About, Contact, Experience, Footer, Hero, Navbar, Projects, StarsCanvas, Tech} from './components';
 
 const App = () => {
+
+  const [toggle, setToggle] = useState(false);
+  
   return (
       <BrowserRouter>
         <div className="relative z-0 bg-black">
-          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-            <Navbar/>
-            <Hero/>
-          </div>
-          <About/>
-          <Projects/>
-          <Tech/>
-          <Experience/>
-          <div className="relative z-0">
-            <Contact/>
-            <StarsCanvas/>
-          </div>
-          <Footer/>
+          <Navbar toggle={toggle} setToggle={setToggle}/>
+            <div onClick={() => setToggle(false)}>
+              <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+                <Hero/>
+              </div>
+              <About/>
+              <Projects/>
+              <Tech/>
+              <Experience/>
+              <div className="relative z-0">
+                <Contact/>
+                <StarsCanvas/>
+              </div>
+              <Footer/>
+            </div>
         </div>
       </BrowserRouter>
   )
