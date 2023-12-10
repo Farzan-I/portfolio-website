bcfbe38e8d14554fc8fcef6bfe2e8e13d43e4cec
Farzan <58693804+Farzan-I@users.noreply.github.com>
2023-12-10 10:30:27 +0000

    Remove unused global colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,24 +7,9 @@ module.exports = {
       colors: {
         primary: "#fe0039",
         secondary: "#e000fe",
-        
-
-        // No reference yet
-        tertiary: "#082744",
-        // #052a4c
-        // #151030 - old tertiary
-
-        // No reference yet
-        quaternary: "#8c0623",
-
-        // For contact background
-        quinary: "#0e0e0ebc",
-
+        tertiary: "#0e0e0ebc",
         "black-100": "#101437",
-        "black-200": "#090325",
-        "dark-grey": "#050505",
         "white-100": "#f0ead6",
-        "yellow-text": "#eda53a",
       },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
